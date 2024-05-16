1f003cb101102bc26666564170e760debe42a417
herdin27 <it06.knitto@gmail.com>
2024-05-16 15:57:52 +0700

    test

--- src/app.js
+++ src/app.js
@@ -9,3 +9,5 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 loadRoutes(path.join(__dirname, 'http/routes'), app);
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+
+// module.exports = app; 
---
+++ tests/index.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../src/app');
+const admin = require('firebase-admin');
+
+// Mock Firebase Admin SDK
+jest.mock('firebase-admin', () => {
+    const firestore = {
+        collection: jest.fn().mockReturnThis(),
+        doc: jest.fn().mockReturnThis(),
+        update: jest.fn().mockResolvedValueOnce({})
+    };
+    return {
+        initializeApp: jest.fn(),
+        credential: {
+            cert: jest.fn()
+        },
+        firestore: jest.fn(() => firestore)
+    };
+});
