363b8426419b9aa4aaba6ec5ee97c63d32c5fa4f
herdin27 <it06.knitto@gmail.com>
2024-05-16 16:36:20 +0700

    change percabangan

--- src/helpers/GenerateCode.helper.js
+++ src/helpers/GenerateCode.helper.js
@@ -8,14 +8,18 @@ exports.GenerateCodeMember = async (db) => {
     const checkLength = LastMember.length > 0 && LastMember.match(/[1-9]/gm)
     console.log(checkLength);
     const addNumber = Number(checkLength) + 1
-    if (checkLength.length === 1) {
-        code = `M00${addNumber}`
+
+    switch (checkLength.length) {
+        case 1:
+            code = `M00${addNumber}`
+            break;
+        case 2:
+            code = `M0${addNumber}`
+            break;
+        case 3:
+            code = `M${addNumber}`
+            break;
     }
-    if (checkLength.length === 2) {
-        code = `M0${addNumber}`
-    }
-    if (checkLength.length === 3) {
-        code = `M${addNumber}`
-    }
+
     return code
 }
